105da77ced681a95d1327891e34067dadb6d5cf5
Nastya808 <57590462+Nastya808@users.noreply.github.com>
2023-10-05 16:33:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Journal
+{
+    public string Title { get; set; }
+    public int NumberOfPages { get; set; }
+    public int NumberOfEmployees { get; set; }
+
+    public Journal(string title, int numberOfPages, int numberOfEmployees)
+    {
+        Title = title;
+        NumberOfPages = numberOfPages;
+        NumberOfEmployees = numberOfEmployees;
+    }
+
+    public void IncreaseEmployees(int amount)
+    {
+        NumberOfEmployees += amount;
+    }
+
+    // Метод для зменшення кількості працівників
+    public void DecreaseEmployees(int amount)
+    {
+        if (NumberOfEmployees - amount >= 0)
+        {
+            NumberOfEmployees -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Неможливо зменшити кількість працівників менше нуля.");
+        }
+    }
+
+    // Перевірка на рівність кількості працівників
+    public bool Equals(Journal other)
+    {
+        return this.NumberOfEmployees == other.NumberOfEmployees;
+    }
+
+    // Перевірка на меншу кількість працівників
+    public bool LessThan(Journal other)
+    {
+        return this.NumberOfEmployees < other.NumberOfEmployees;
+    }
+
+    // Перевірка на більшу кількість працівників
+    public bool GreaterThan(Journal other)
+    {
+        return this.NumberOfEmployees > other.NumberOfEmployees;
+    }
+}
